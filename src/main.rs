e226a48bab2848c204d6301fe83cc036716e683c
Paul Vautier <vautier.paul14350@gmail.com>
2024-02-19 08:37:48 +0100

    video decoding. Must not abord on eagain

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::{ffi::CString, ptr::null_mut};
+use std::{
+    ffi::{c_void, CStr, CString},
+    ptr::{null, null_mut},
+};
 
-use ffmpeg4_ffi::sys::{self, AVMediaType_AVMEDIA_TYPE_VIDEO};
-use ffmpeg4_ffi::sys::AVFormatContext;
+use ffmpeg4_ffi::{
+    extra::defs::{averror, averror_eof, eagain},
+    sys::{self, AVHWFramesContext, AVMediaType_AVMEDIA_TYPE_VIDEO},
+};
 
 trait ToU32Result {
     fn to_u32_result(self, err_str: &str) -> Result<u32, String>;
@@ -12,7 +17,17 @@ impl ToU32Result for i32 {
         if self >= 0 {
             Ok(self as u32)
         } else {
-            Err(err_str.to_string())
+            let mut description: [std::os::raw::c_char; sys::AV_ERROR_MAX_STRING_SIZE as usize] =
+                [0; sys::AV_ERROR_MAX_STRING_SIZE as usize];
+            let res: &CStr = unsafe {
+                sys::av_strerror(
+                    self,
+                    description.as_mut_ptr(),
+                    sys::AV_ERROR_MAX_STRING_SIZE as usize,
+                );
+                CStr::from_ptr(description.as_ptr())
+            };
+            Err(format!("{}. {}", res.to_string_lossy(), err_str))
         }
     }
 }
@@ -22,24 +37,38 @@ struct VideoDecoder {
     pkt: *mut sys::AVPacket,
     codec: *mut sys::AVCodec,
     codec_ctx: *mut sys::AVCodecContext,
+    frame: *mut sys::AVFrame,
+    video_stream_idx: u32,
 }
 
+enum VideoResult {
+    Finished,
+    Decoded,
+}
+
+fn to_cstring(str: &str) -> CString {
+    CString::new(str).expect("could not create cstring")
+}
 impl VideoDecoder {
     fn new(filename: &str) -> Result<Self, String> {
         unsafe {
-            let mut fmt_ctx = sys::avformat_alloc_context()
+        sys::avdevice_register_all();
+            let frame: *mut sys::AVFrame = sys::av_frame_alloc();
+            let fmt_ctx = sys::avformat_alloc_context()
                 .as_mut()
                 .ok_or("Could not aquire format context")?;
-            let c_filename = CString::new(filename).expect("").as_ptr();
+
+            let mut options: *mut sys::AVDictionary = null_mut();
+            fmt_ctx.iformat = sys::av_find_input_format(to_cstring("v4l2").as_ptr());
             sys::avformat_open_input(
                 &mut (fmt_ctx as *mut sys::AVFormatContext),
-                c_filename,
+                to_cstring(filename).as_ptr(),
                 null_mut(),
                 null_mut(),
             )
             .to_u32_result(format!("{} : {}", "could not open file", filename).as_str())?;
 
-            let mut pkt = sys::av_packet_alloc()
+            let pkt = sys::av_packet_alloc()
                 .as_mut()
                 .ok_or("Could not aquire a pkt")?;
 
@@ -52,30 +81,68 @@ impl VideoDecoder {
                 0,
             )
             .to_u32_result("could not find video stream")?;
-
-            let mut codec = sys::avcodec_find_decoder(best_stream_idx as u32)
+            println!("{best_stream_idx} {}", best_stream_idx as isize);
+            let codec_params = (**(fmt_ctx.streams.wrapping_add(best_stream_idx as usize))).codecpar;
+        
+            println!("dasda");
+            let codec = sys::avcodec_find_decoder((*codec_params).codec_id)
                 .as_mut()
                 .ok_or("Could not aquire a codec")?;
-
-            let mut codec_ctx = sys::avcodec_alloc_context3(codec)
+            println!("aaa");
+            let codec_ctx = sys::avcodec_alloc_context3(codec)
                 .as_mut()
                 .ok_or("Could not aquire a codec context")?;
+            codec_ctx.pix_fmt = sys::AVPixelFormat_AV_PIX_FMT_RGB24;
 
+            sys::avcodec_parameters_to_context(codec_ctx, codec_params);
             sys::avcodec_open2(codec_ctx, codec, null_mut())
                 .to_u32_result("Could not open codec")?;
-
             Ok(VideoDecoder {
                 fmt_ctx,
                 pkt,
                 codec,
                 codec_ctx,
+                frame,
+                video_stream_idx: best_stream_idx,
             })
         }
     }
+
+    fn decode_frames<F>(&mut self, output_file: &str, on_frame_decoded: F) -> Result<(), String>
+    where
+        F: Fn(*mut sys::AVFrame),
+    {
+        unsafe {
+            while sys::av_read_frame(self.fmt_ctx, self.pkt) >= 0 {
+                if (*self.pkt).stream_index as u32 == self.video_stream_idx {
+                    sys::avcodec_send_packet(self.codec_ctx, self.pkt)
+                        .to_u32_result("Could not send packet to the AVCodec of the decoder")?;
+                    loop {
+                        let ret = sys::avcodec_receive_frame(self.codec_ctx, self.frame)
+                            .to_u32_result("error while receiving a frame from the decoder")?;
+                        on_frame_decoded(self.frame);
+                    }
+
+                }
+            }
+        }
+        Ok(())
+    }
 }
-fn main() -> Result<(), &'static str> {
-    let input: *mut sys::AVFrame = unsafe { sys::av_frame_alloc() };
-    let output: *mut sys::AVFrame = unsafe { sys::av_frame_alloc() };
-    unsafe { loop {} }
+
+impl Drop for VideoDecoder {
+    fn drop(&mut self) {
+        unsafe {
+            sys::av_frame_free(&mut self.frame);
+        }
+    }
+}
+fn main() -> Result<(), String> {
+    let mut decoder = VideoDecoder::new("/dev/video0")?;
+    println!("o");
+    decoder.decode_frames("", |frame| unsafe {
+        println!("frame format {}", (*frame).format);
+    })?;
+
     Ok(())
 }
